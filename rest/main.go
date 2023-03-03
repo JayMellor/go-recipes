b72574be690f52f779788f8e7d33fd700db2734a
Jay Mellor <jaymellor@Jays-MacBook-Pro.local>
2023-03-03 18:51:58 +0000

    Added regex and path argument support to urls

--- rest/main.go
+++ rest/main.go
@@ -6,47 +6,105 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
+	"strconv"
 )
 
 const port string = ":8080"
 
-var stock = map[string]int{
-	"Trousers": 0,
-	"Jackets":  0,
+type Product struct {
+	id          int
+	description string
+	stockLevel  int
 }
+
+var stock = []Product{
+	{
+		id:          1,
+		description: "Jacket",
+		stockLevel:  0,
+	},
+	{
+		id:          2,
+		description: "Shoes",
+		stockLevel:  0,
+	},
+}
+
+type HandlerFunc func(request *http.Request, args map[string]string) (response string, status int)
 
 type Endpoint struct {
 	route    string
-	handlers map[string]func(request *http.Request) (response string, status int)
+	handlers map[string]HandlerFunc
 }
 
-var routes = []Endpoint{{
-	route: `^/$`,
-	handlers: map[string]func(request *http.Request) (response string, status int){
-		http.MethodGet: func(request *http.Request) (response string, status int) {
-			return "hello!", http.StatusOK
+var routes = []Endpoint{
+	{
+		route: `^/$`,
+		handlers: map[string]HandlerFunc{
+			http.MethodGet: func(request *http.Request, _ map[string]string) (response string, status int) {
+				return "hello!", http.StatusOK
+			},
 		},
 	},
-}}
+	{
+		route: `^/stock/?$`,
+		handlers: map[string]HandlerFunc{
+			http.MethodGet: func(request *http.Request, _ map[string]string) (response string, status int) {
+				return fmt.Sprint(stock), http.StatusOK
+			},
+		},
+	},
+	{
+		route: `^/stock/(?P<productId>\d+)/?$`,
+		handlers: map[string]HandlerFunc{
+			http.MethodGet: func(request *http.Request, args map[string]string) (response string, status int) {
+				id, error := strconv.Atoi(args["productId"])
+				if error != nil {
+					return fmt.Sprint(error), http.StatusBadRequest
+				}
 
-func find_endpoint(URL *url.URL, method string) (endpoint *Endpoint, error error) {
+				for _, product := range stock {
+					if product.id == id {
+						return fmt.Sprint(product), http.StatusOK
+					}
+				}
+				return fmt.Sprintln("No product with the following ID found:", id), http.StatusNotFound
+			},
+		},
+	},
+}
+
+func findNamedStringSubmatch(regex *regexp.Regexp, url string) (namedMatches map[string]string, found bool) {
+	matches := regex.FindStringSubmatch(url)
+	if len(matches) == 0 {
+		return namedMatches, false
+	}
+
+	namedMatches = make(map[string]string)
+	for place, name := range regex.SubexpNames() {
+		namedMatches[name] = matches[place]
+	}
+	return namedMatches, true
+}
+
+func find_endpoint(URL *url.URL, method string) (endpoint *Endpoint, matches map[string]string, error error) {
 	for _, endpoint := range routes {
-		matched, error := regexp.Match(endpoint.route, []byte(fmt.Sprint(URL)))
+		routeExpr, error := regexp.Compile(endpoint.route)
 		if error != nil {
-			return nil, error
+			return nil, nil, error
 		}
 
-		if matched {
-			return &endpoint, nil
+		if namedMatches, found := findNamedStringSubmatch(routeExpr, URL.String()); found {
+			return &endpoint, namedMatches, nil
 		}
 
 	}
-	return nil, fmt.Errorf("No endpoints matching %v were found", URL)
+	return nil, nil, fmt.Errorf("No endpoints matching %v were found", URL)
 }
 
 func match_endpoint(writer http.ResponseWriter, request *http.Request) {
 
-	endpoint, error := find_endpoint(request.URL, request.Method)
+	endpoint, matches, error := find_endpoint(request.URL, request.Method)
 	if error != nil {
 		writer.WriteHeader(http.StatusNotFound)
 		writer.Write([]byte(fmt.Sprint(error)))
@@ -57,7 +115,7 @@ func match_endpoint(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusMethodNotAllowed)
 		writer.Write([]byte(fmt.Sprintf("Method %v is not allowed for endpoint %v", request.Method, request.URL)))
 	} else {
-		response, status := handler(request)
+		response, status := handler(request, matches)
 		if status != http.StatusOK {
 			writer.WriteHeader(status)
 		}
@@ -69,30 +127,7 @@ func match_endpoint(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
-
 	http.HandleFunc("/", match_endpoint)
-
-	// http.HandleFunc("/stock", func(writer http.ResponseWriter, request *http.Request) {
-
-	// 	switch method := request.Method; method {
-	// 	case http.MethodGet:
-	// 		for product, stockLevel := range stock {
-	// 			writer.Write([]byte(fmt.Sprint(product, ":", stockLevel, "<br/>")))
-	// 		}
-
-	// 	case http.MethodPost:
-
-	// 	default:
-	// writer.WriteHeader(http.StatusMethodNotAllowed)
-	// writer.Write([]byte(fmt.Sprintf("Method %v is not allowed for endpoint %v", method, request.URL)))
-	// 	}
-	// })
-
-	// http.HandleFunc("/stock/:id",
-	// 	func(writer http.ResponseWriter, request *http.Request) {
-	// 		writer.Write([]byte(fmt.Sprint("Url is", request.URL)))
-	// 	})
-
 	fmt.Println("Listening on port", port)
 	if error := http.ListenAndServe(port, nil); error != nil {
 		log.Fatal(error)
