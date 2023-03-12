83a3f8793dfd12f95efc342451e352630b012386
Jay Mellor <jaymellor@Jays-MacBook-Pro.local>
2023-03-12 19:02:40 +0000

    Updated request validation on creation. Updated URLs

--- rest/endpoint.go
+++ rest/endpoint.go
@@ -21,40 +21,93 @@ func getProducts(request *http.Request, _ map[string]string) (response string, s
 	return string(productJson), http.StatusOK
 }
 
+func requestBody(request *http.Request) (body map[string]any, error error) {
+	jsonBody, error := io.ReadAll(request.Body)
+	if error != nil {
+		return body, fmt.Errorf("Error reading body: %w", error)
+	}
+	if error := json.Unmarshal(jsonBody, &body); error != nil {
+		return body, fmt.Errorf("Error parsing request body: %w", error)
+	}
+
+	return body, nil
+}
+
+type Validator func(value any) error
+
+type Field struct {
+	validator Validator
+	optional  bool
+}
+
+func missingFieldError(missingFields []string) error {
+	message := ""
+	for _, missingField := range missingFields {
+		message += fmt.Sprintf("[%v]: field is required\n", missingField)
+	}
+	return fmt.Errorf("%s", message)
+}
+
+func validateBody(body map[string]any, fields map[string]Field) error {
+
+	missingFields := []string{}
+	invalidFields := map[string]error{}
+	for fieldName, fieldProps := range fields {
+		if value, fieldExists := body[fieldName]; fieldExists {
+			if error := fieldProps.validator(value); error != nil {
+				invalidFields[fieldName] = error
+			}
+		} else {
+			missingFields = append(missingFields, fieldName)
+		}
+	}
+
+	if len(missingFields) > 0 {
+		return missingFieldError(missingFields)
+	}
+	if len(invalidFields) > 0 {
+		return fmt.Errorf("Fields were invalid: %v", invalidFields)
+	}
+
+	return nil
+}
+
 func createProduct(request *http.Request, _ map[string]string) (response string, status int) {
 
-	body, error := io.ReadAll(request.Body)
-	if error != nil {
-		return fmt.Sprintln("Error reading body:", error), http.StatusInternalServerError
-	}
-	bodyRequest := map[string]any{}
-	if error := json.Unmarshal(body, &bodyRequest); error != nil {
-		return fmt.Sprintln("Error parsing request", error), http.StatusBadRequest
+	body, err := requestBody(request)
+	if err != nil {
+		return fmt.Sprint(err), http.StatusInternalServerError
 	}
 
-	description, descriptionExists := bodyRequest["Description"]
-	stockLevel, stockLevelExists := bodyRequest["StockLevel"]
-	missingFields := []string{}
-	if !descriptionExists {
-		missingFields = append(missingFields, "Description")
-	}
-	if !stockLevelExists {
-		missingFields = append(missingFields, "Stock Level")
+	var fields = map[string]Field{
+		"Description": Field{
+			validator: func(value any) error {
+				return nil
+			},
+			optional: false,
+		},
+		"StockLevel": Field{
+			validator: func(value any) error {
+				if stockLevel, err := strconv.Atoi(fmt.Sprint(value)); err != nil {
+					return fmt.Errorf("Stock Level must be an integer. Received %v", stockLevel)
+				}
+				return nil
+			},
+			optional: false,
+		},
 	}
 
-	if !descriptionExists || !stockLevelExists {
-		return fmt.Sprintln("The following fields are required:", missingFields), http.StatusBadRequest
+	// todo combine validation and deserialization/parsing somehow?
+	if err := validateBody(body, fields); err != nil {
+		return fmt.Sprint(err), http.StatusBadRequest
 	}
-
-	stkLvl, error := strconv.Atoi(fmt.Sprint(stockLevel))
-	if error != nil {
-		return fmt.Sprintln("Stock Level must be an integer. Received", stockLevel), http.StatusBadRequest
-	}
+	stockLevel, _ := strconv.Atoi(fmt.Sprint(body["StockLevel"]))
+	description := body["Description"]
 
 	newProduct := Product{
 		Id:          stock[len(stock)-1].Id + 1,
 		Description: fmt.Sprint(description),
-		StockLevel:  stkLvl,
+		StockLevel:  stockLevel,
 	}
 
 	stock = append(stock, newProduct)
@@ -88,25 +141,51 @@ func getProduct(request *http.Request, args map[string]string) (response string,
 
 }
 
+func updateProduct(request *http.Request, args map[string]string) (response string, status int) {
+	id, error := strconv.Atoi(args["productId"])
+	if error != nil {
+		return fmt.Sprint(error), http.StatusBadRequest
+	}
+
+	match, found := findProduct(id)
+	if !found {
+		return fmt.Sprintln("No product with the following ID found:", id),
+			http.StatusNotFound
+	}
+
+	productJson, error := json.Marshal(match)
+	if error != nil {
+		return fmt.Sprint(error), http.StatusInternalServerError
+	}
+
+	return string(productJson), http.StatusOK
+}
+
+// // One possibility to control access to routes
+// func PopulateRoutes(endpoints []Endpoint) {
+// 	routes = endpoints
+// }
+
 // add \?.* to end of routes to allow query params
 var Routes = []Endpoint{
 	{
 		route: `^/$`,
 		handlers: map[string]HandlerFunc{
 			http.MethodGet: func(request *http.Request, _ map[string]string) (response string, status int) {
+				// return all endpoints
 				return "hello!", http.StatusOK
 			},
 		},
 	},
 	{
-		route: `^/stock/?$`,
+		route: `^/products/?$`,
 		handlers: map[string]HandlerFunc{
 			http.MethodGet:  getProducts,
 			http.MethodPost: createProduct,
 		},
 	},
 	{
-		route: `^/stock/(?P<productId>\d+)/?$`,
+		route: `^/products/(?P<productId>\d+)/?$`,
 		handlers: map[string]HandlerFunc{
 			http.MethodGet: getProduct,
 		},
